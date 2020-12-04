54f420236d27c02fe1aae56e4ef94d4a4ac749f8
jamwitk <hushdilcss@gmail.com>
2020-12-05 00:07:14 +0300

    Now zooming with ScrollWheel but need optimization

---
+++ Assets/Scripts/testing.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testing : MonoBehaviour
+{
+  
+    void Start()
+    {
+        
+    }
+
+ 
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime);
+    }
+}
