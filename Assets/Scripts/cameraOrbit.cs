d1838f8d835d4aab59c2f941c85d8affa74f8ea8
jamwitk <hushdilcss@gmail.com>
2020-12-23 21:36:10 +0300

    Now it's rotating in Y Axis and Zooming smoothly

---
+++ Assets/Scripts/cameraOrbit.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraOrbit : MonoBehaviour
+{
+    public Transform target;
+    public float distance = 5.0f;
+    public float xSpeed = 120.0f;
+    public float ySpeed = 120.0f;
+ 
+    public float yMinLimit = -20f;
+    public float yMaxLimit = 80f;
+ 
+    public float distanceMin = .5f;
+    public float distanceMax = 15f;
+ 
+    private Rigidbody rigidbody;
+ 
+    float x = 0.0f;
+    float y = 0.0f;
+    
+    //Smooth Zoom effect
+    float _currentZoom = 5f; //Setting default distance value
+    float _zoomTime = 5f;
+    
+    // Use this for initialization
+    void Start () 
+    {
+        Vector3 angles = transform.eulerAngles;
+        x = angles.y;
+        y = angles.x;
+ 
+        rigidbody = GetComponent<Rigidbody>();
+ 
+        // Make the rigid body not change rotation
+        if (rigidbody != null)
+        {
+            rigidbody.freezeRotation = true;
+        }
+    }
+ 
+    void LateUpdate () 
+    {
+        if (target) 
+        {
+            if (Input.GetMouseButton(0))
+            {
+                x += Input.GetAxis("Mouse X") * xSpeed * distance * 0.02f;
+                y -= Input.GetAxis("Mouse Y") * ySpeed * 0.02f;
+ 
+                y = ClampAngle(y, yMinLimit, yMaxLimit);
+                
+            }
+            distance = Mathf.Clamp(distance - Input.GetAxis("Mouse ScrollWheel")*5, distanceMin, distanceMax);
+            _currentZoom = Mathf.Lerp(_currentZoom, distance, _zoomTime * Time.deltaTime);// Smoothing zoom 
+            RaycastHit hit;
+            if (Physics.Linecast (target.position, transform.position, out hit)) 
+            {
+                distance -=  hit.distance;
+            }
+            Quaternion rotation = Quaternion.Euler(y, x, 0);
+            
+            Vector3 negDistance = new Vector3(0.0f, 0.0f, -_currentZoom);
+            Vector3 position = rotation * negDistance + target.position;
+
+            transform.rotation = rotation;
+            transform.position = position;
+        }
+    }
+ 
+    public static float ClampAngle(float angle, float min, float max)
+    {
+        if (angle < -360F)
+            angle += 360F;
+        if (angle > 360F)
+            angle -= 360F;
+        return Mathf.Clamp(angle, min, max);
+    }
+}
